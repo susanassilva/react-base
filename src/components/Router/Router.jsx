fa481881eeeb1936fab04d73c7e4d21043611bdd
Susana S Silva <susana_santos@id.uff.br>
2022-04-14 12:22:37 -0300

    'react-router-dom'

---
+++ src/components/Router/Router.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from '../../pages/Home';
+import Login from '../../pages/Login';
+import Register from '../../pages/Register';
+
+const Router = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  </BrowserRouter>
+);
+export default Router;
